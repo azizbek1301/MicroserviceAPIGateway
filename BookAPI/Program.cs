57a7d93d7a09b2658f1c07d7d424ba7907f1a94c
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 18:32:46 +0500

    update

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,3 +1,8 @@
+using BookAPI.DataAcsess;
+using BookAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 
 var app = builder.Build();
 
