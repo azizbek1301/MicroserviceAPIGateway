9839b4d94447dd67b93b04a4ebd6f3ee69425cb1
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 18:32:29 +0500

    Book services added

---
+++ BookAPI/Services/BookService.cs
@@ -0,0 +1,33 @@
+﻿using BookAPI.DTOs;
+using BookAPI.Models;
+
+namespace BookAPI.Services
+{
+    public class BookService : IBookService
+    {
+        public ValueTask<bool> CreateBookAsync(BookDto book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<List<Book>> GetAllBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Book> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<string> UpdateBookAsync(int id, BookDto book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookAPI/Services/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BookAPI.DTOs;
+using BookAPI.Models;
+
+namespace BookAPI.Services
+{
+    public interface IBookService
+    {
+        public ValueTask<bool> CreateBookAsync (BookDto book);
+        public ValueTask<List<Book>> GetAllBooksAsync ();
+        public ValueTask<Book> GetByIdAsync (int id);
+        public ValueTask<string>DeleteByIdAsync(int id);
+        public ValueTask<string> UpdateBookAsync(int id, BookDto book);
+
+    }
+}
