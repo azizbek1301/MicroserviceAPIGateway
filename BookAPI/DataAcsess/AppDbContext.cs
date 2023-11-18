ca0654ddfabc02a6cebd7284a7923d8909ed70db
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 18:32:01 +0500

    AppDbContext added

---
+++ BookAPI/DataAcsess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.DataAcsess
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
