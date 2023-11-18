8f1863525b85946dd2d60c3ca385789326e16b16
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 09:37:05 +0500

    ApplicationDbContext

---
+++ AuthorAPI/ApplicationDbContext/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using AuthorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorAPI.ApplicationDbContext
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { }
+        
+        public virtual DbSet<Author>Authors { get; set; }
+    }
+}
