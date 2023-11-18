51784676e7549a3a885449dab2213fd94eb0d0fc
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 18:32:20 +0500

    book model created

---
+++ BookAPI/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace BookAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public decimal Price { get; set; }
+    }
+}
