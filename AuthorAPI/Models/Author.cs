a2433d465bc807b00496e3ffe03944abed0a8ed3
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 09:43:49 +0500

    Author models created

---
+++ AuthorAPI/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthorAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+        public string Email {  get; set; }
+       
+    }
+}
