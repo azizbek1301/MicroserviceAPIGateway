f3556dd505eed4ec75073a57a77931195b169eb2
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 18:31:48 +0500

    book controller

---
+++ BookAPI/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
