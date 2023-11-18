e94f5ae4ee96bb26e89b012603a6c0117658e81d
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-18 09:55:22 +0500

    AuthorDto model created

---
+++ AuthorAPI/DTOs/AuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthorAPI.DTOs
+{
+    public class AuthorDto
+    {
+        public string Name {  get; set; }
+        public string Surname {  get; set; }
+        public string Email { get; set; }
+    }
+}
