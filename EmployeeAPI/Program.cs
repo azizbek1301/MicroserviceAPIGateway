b10ef178d7bba80a7b4329edb1ac76ccb6e77a68
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 16:56:20 +0500

    Program.cs update connect

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -1,3 +1,9 @@
+using System.Text.Json.Serialization;
+using System;
+using EmployeeAPI.DataAcsess;
+using Microsoft.EntityFrameworkCore;
+using EmployeeAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
