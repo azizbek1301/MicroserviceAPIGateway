850cb1785cc22739f1dee783e8421e93b196c91a
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 17:59:14 +0500

    Library services added

---
+++ LibraryAPI/Services/ILibrarySerice.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAPI.DTOs;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Services
+{
+    public interface ILibrarySerice
+    {
+        public ValueTask<bool> CreateLibraryAsync(LibraryDto libraryDto);
+        public ValueTask<List<Library>> GetAllLibraryAsync();
+        public ValueTask<Library> GetByIdAsync(int id);
+        public ValueTask<string> DeleteByIdAsync(int id);
+        public ValueTask<string> UpdateLibraryAsync(int id, LibraryDto libraryDto);
+    }
+}
---
+++ LibraryAPI/Services/LibraryService.cs
@@ -0,0 +1,110 @@
+﻿using LibraryAPI.DataAcsess;
+using LibraryAPI.DTOs;
+using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Services
+{
+    public class LibraryService : ILibrarySerice
+    {
+        private readonly AppDbContext _context;
+        public LibraryService(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public async ValueTask<bool> CreateLibraryAsync(LibraryDto libraryDto)
+        {
+            try
+            {
+
+                var res = new Library()
+                {
+                    Name = libraryDto.Name,
+                    
+                };
+                await _context.Libraries.AddAsync(res);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async ValueTask<string> DeleteByIdAsync(int id)
+        {
+            try
+            {
+                var res = await _context.Libraries.FirstOrDefaultAsync(x => x.Id == id);
+                if (res != null)
+                {
+                    _context.Libraries.Remove(res);
+                    await _context.SaveChangesAsync();
+                    return "o'chirildi";
+
+                }
+                else
+                {
+                    return "Not found";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public async ValueTask<List<Library>> GetAllLibraryAsync()
+        {
+            try
+            {
+                var res= await _context.Libraries.AsNoTracking().ToListAsync();
+                return res;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async ValueTask<Library> GetByIdAsync(int id)
+        {
+            try
+            {
+                var res = await _context.Libraries.FirstOrDefaultAsync(x => x.Id == id);
+                return res;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async ValueTask<string> UpdateLibraryAsync(int id, LibraryDto libraryDto)
+        {
+            try
+            {
+                var res = await _context.Libraries.FirstOrDefaultAsync(x => x.Id == id);
+                if (res != null)
+                {
+                    res.Name = libraryDto.Name;
+                    
+                    await _context.SaveChangesAsync();
+                    return "Yangilandi";
+                }
+                else
+                {
+                    return "Topilmadi";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
