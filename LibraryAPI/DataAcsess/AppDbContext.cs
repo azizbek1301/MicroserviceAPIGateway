fd6517343c5c0b1fc6606f6103866dc0b1e3d80c
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 17:58:31 +0500

    AppDbContext added

---
+++ LibraryAPI/DataAcsess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.DataAcsess
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        
+        public virtual DbSet<Library> Libraries { get; set; }
+    }
+}
