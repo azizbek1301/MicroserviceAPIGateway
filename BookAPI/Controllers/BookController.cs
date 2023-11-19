4da3ba80825b83faf6f0089fbe53f0b9361e77b1
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 15:25:30 +0500

    BookController Added

--- BookAPI/Controllers/BookController.cs
+++ BookAPI/Controllers/BookController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookAPI.DTOs;
+using BookAPI.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+            
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateBookAsync(BookDto bookDto)
+        {
+            var res=_bookService.CreateBookAsync(bookDto);
+            return Ok(res);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var res=await _bookService.GetAllBooksAsync();
+            return Ok(res);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var res = await _bookService.GetByIdAsync(id);
+            return Ok(res);
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteByIdAsync(int id)
+        {
+            var res=await _bookService.DeleteByIdAsync(id);
+            return Ok(res);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateBookAsync(int id, BookDto bookDto)
+        {
+            var res=await _bookService.UpdateBookAsync(id, bookDto);
+            return Ok(res);
+        }
     }
 }
