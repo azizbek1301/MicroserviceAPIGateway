49c791bdfe1d37b3218a224612256e4d3c020d3b
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 16:55:15 +0500

    AppDbContext

---
+++ EmployeeAPI/DataAcsess/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.DataAcsess
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        
+
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
