df79e3fc5b0a1185f4ae98c5277e8a99944227f5
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-19 16:55:30 +0500

    DTOs

---
+++ EmployeeAPI/DTOs/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace EmployeeAPI.DTOs
+{
+    public class EmployeeDto
+    {
+        public string Name {  get; set; }
+        public decimal Price { get; set; }
+    }
+}
